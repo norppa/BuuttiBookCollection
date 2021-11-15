aab9a0dc50b72aa736f5a8d5f20a16284a528f28
Jari Haavisto <jari.haavisto@gmail.com>
2021-11-15 10:06:02 +0200

    add sqlite3 database

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const config = require('config')
+
+const PORT = config.get('port')
+const db = require('better-sqlite3')(config.get('database'))
 
 const server = express()
 
 server.get('/', (req, res) => {
-    res.send('Buutti Book Collection Backend')
+    const result = db.prepare('SELECT * FROM books').all()
+    res.send(result)
 })
 
 server.listen(3000, () => console.log('Buutti Book Collection running'))
